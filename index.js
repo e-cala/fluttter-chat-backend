4e68b87e19f395e8bddcea49a6f6e5135837f0d4
Eric <93eric@gmail.com>
2020-11-01 08:52:32 +0100

    Fin sección 8

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express'); //importar express
+const path = require('path');
+require('dotenv').config();
+
+//App de Express
+const app = express(); //inicializar aplicación
+
+//Lectura y parseo del Body
+app.use( express.json() );
+
+//DB Config
+require('./database/config').dbConnection();
+
+
+//Node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
+
+
+
+//path publico
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+
+
+//Mis Rutas
+app.use( '/api/login', require('./routes/auth') );
+
+
+
+
+server.listen(process.env.PORT, (err) => {
+    if (err) throw new Error(err);
+    console.log('Servidor corriendo en puerto', process.env.PORT);
+});
+
